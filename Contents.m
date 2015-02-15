f23736c44e3cb68869633cb0149b2fea4540ede6
Pascal COMBES <pascom@orange.fr>
2015-02-15 15:40:32 +0100

    A Contents.m help index file.
    Provides a hyper-linked summary of this folder files.

---
+++ Contents.m
@@ -0,0 +1,7 @@
+%% CODE MATLAB Small (or even quite big) utilities scripts
+%
+% Files:
+%    strjoin - Glue all string in cell with glue.
+%    stringSplit - Splits a string into parts according to delimiter.
+%    chdir - Equivent to cd with a history.
+%    fullpath - Returns the absolute path to the path given.
